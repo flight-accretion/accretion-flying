990418ffcfed33ab056de9bac6557834f0749e11
jitendra kumar jat <jitendra@skyrack.ai>
2026-07-10 19:05:40 +0530

    added

--- airports_web-master-flight/app/Http/routes.php
+++ airports_web-master-flight/app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,86 +12,103 @@
 |
 */
 
+
 if (! function_exists('legacy_controller_routes')) {
-  function legacy_controller_routes($prefix, $controller)
-  {
-    $controller_class = 'FlyingCalculation\\Http\\Controllers\\' . $controller;
-    $reflection = new ReflectionClass($controller_class);
+ function legacy_controller_routes($prefix, $controller)
+ {
+   $controller_class = 'FlyingCalculation\\Http\\Controllers\\' . $controller;
+   $reflection = new ReflectionClass($controller_class);
 
-    foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-      if ($method->getDeclaringClass()->getName() !== $controller_class) {
-        continue;
-      }
 
-      $method_name = $method->getName();
+   foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+     if ($method->getDeclaringClass()->getName() !== $controller_class) {
+       continue;
+     }
 
-      if ($method->isConstructor() || ! preg_match('/^(get|post|put|patch|delete|any)(.+)$/', $method_name, $matches)) {
-        continue;
-      }
 
-      $verb = strtolower($matches[1]);
-      $action = $matches[2];
-      $uri = trim($prefix, '/');
+     $method_name = $method->getName();
 
-      if ($action !== 'Index') {
-        $uri .= '/' . Illuminate\Support\Str::kebab($action);
-      }
 
-      foreach ($method->getParameters() as $parameter) {
-        $type = $parameter->getType();
-        if ($type instanceof ReflectionNamedType && ! $type->isBuiltin()) {
-          continue;
-        }
+     if ($method->isConstructor() || ! preg_match('/^(get|post|put|patch|delete|any)(.+)$/', $method_name, $matches)) {
+       continue;
+     }
 
-        $uri .= '/{' . $parameter->getName() . '?}';
-      }
 
-      Route::$verb($uri, $controller . '@' . $method_name);
-    }
-  }
+     $verb = strtolower($matches[1]);
+     $action = $matches[2];
+     $uri = trim($prefix, '/');
+
+
+     if ($action !== 'Index') {
+       $uri .= '/' . Illuminate\Support\Str::kebab($action);
+     }
+
+
+     foreach ($method->getParameters() as $parameter) {
+       $type = $parameter->getType();
+       if ($type instanceof ReflectionNamedType && ! $type->isBuiltin()) {
+         continue;
+       }
+
+
+       $uri .= '/{' . $parameter->getName() . '?}';
+     }
+
+
+     Route::$verb($uri, $controller . '@' . $method_name);
+   }
+ }
 }
+
 
 foreach ([
-  'user' => 'UserController',
-  'mail-content' => 'MailContentController',
-  'booking' => 'BookingController',
-  'admin' => 'AdminController',
-  'plane' => 'PlaneController',
-  'subtype' => 'PlaneSubtypeController',
-  'owner' => 'OwnerController',
-  'setting' => 'SettingController',
-  'home' => 'HomeController',
-  'route' => 'RouteController',
-  'city' => 'CityController',
-  'airport' => 'AirportController',
-  'password' => 'Auth\PasswordController',
+ 'user' => 'UserController',
+ 'mail-content' => 'MailContentController',
+ 'booking' => 'BookingController',
+ 'admin' => 'AdminController',
+ 'plane' => 'PlaneController',
+ 'subtype' => 'PlaneSubtypeController',
+ 'owner' => 'OwnerController',
+ 'setting' => 'SettingController',
+ 'home' => 'HomeController',
+ 'route' => 'RouteController',
+ 'city' => 'CityController',
+ 'airport' => 'AirportController',
+ 'password' => 'Auth\PasswordController',
 ] as $prefix => $controller) {
-  legacy_controller_routes($prefix, $controller);
+ legacy_controller_routes($prefix, $controller);
 }
 
+
 Route::get('/uploads/{path}', function ($path) {
-  $uploads_root = realpath(public_path('uploads'));
-  $normalized_path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
-  $file_path = public_path('uploads' . DIRECTORY_SEPARATOR . $normalized_path);
-  $real_file_path = realpath($file_path);
+ $uploads_root = realpath(public_path('uploads'));
+ $normalized_path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+ $file_path = public_path('uploads' . DIRECTORY_SEPARATOR . $normalized_path);
+ $real_file_path = realpath($file_path);
 
-  if($uploads_root && $real_file_path && is_file($real_file_path) && strpos($real_file_path, $uploads_root) === 0) {
-    return response()->file($real_file_path, ['Cache-Control' => 'public, max-age=86400']);
-  }
 
-  if(app()->environment('local')) {
-    $remote_uploads_url = rtrim(env('REMOTE_UPLOADS_URL', 'https://fliying.airaccretion.com/uploads'), '/');
-    $remote_path = implode('/', array_map('rawurlencode', explode('/', str_replace('\\', '/', $path))));
-    return redirect()->away($remote_uploads_url . '/' . $remote_path);
-  }
+ if($uploads_root && $real_file_path && is_file($real_file_path) && strpos($real_file_path, $uploads_root) === 0) {
+   return response()->file($real_file_path, ['Cache-Control' => 'public, max-age=86400']);
+ }
 
-  $placeholder = public_path('img/plane-2image.jpg');
-  if(!is_file($placeholder)) {
-    $placeholder = public_path('images/unknown.jpg');
-  }
 
-  return response()->file($placeholder, ['Cache-Control' => 'public, max-age=300']);
+ if(app()->environment('local')) {
+   $remote_uploads_url = rtrim(env('REMOTE_UPLOADS_URL', 'https://fliying.airaccretion.com/uploads'), '/');
+   $remote_path = implode('/', array_map('rawurlencode', explode('/', str_replace('\\', '/', $path))));
+   return redirect()->away($remote_uploads_url . '/' . $remote_path);
+ }
+
+
+ $placeholder = public_path('img/plane-2image.jpg');
+ if(!is_file($placeholder)) {
+   $placeholder = public_path('images/unknown.jpg');
+ }
+
+
+ return response()->file($placeholder, ['Cache-Control' => 'public, max-age=300']);
 })->where('path', '.*');
+
+
 
 
 Route::get('/my-bookings', 'BookingController@getMy');
@@ -104,3 +122,6 @@ Route::get('/plane_list', 'HomeController@getPlaneList');
 Route::get('/handling-charge', 'CityController@getHandlingCharge');
 Route::post('/handling-charge', 'CityController@postHandlingCharge');
 Route::get('/', 'HomeController@index');
+
+
+
